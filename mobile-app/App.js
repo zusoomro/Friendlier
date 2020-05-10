6e0840c6b0634c3041f237f3ee306a7bf64ab39c
zusoomro <zusoomro@seas.upenn.edu>
2020-05-09 22:44:47 -0400

    init
    changes procfile directory
    
    changes procfile path
    
    Create api-test.yml
    
    Update api-test.yml
    
    Adds .gitignore
    
    init django api, mobile app

---
+++ mobile-app/App.js
@@ -0,0 +1,42 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import { Text, View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Details">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to the details screen"
+        onPress={() => navigation.navigate('Details')}
+      ></Button>
+    </View>
+  );
+}
+
+function DetailsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+      <Button
+        title="Go to the home screen"
+        onPress={() => navigation.navigate('Home')}
+      ></Button>
+    </View>
+  );
+}
---
+++ mobile-app/env.js
@@ -0,0 +1,7 @@
+const env = {
+  dev: {
+    apiUrl: 'http://localhost:8000',
+  },
+};
+
+export default env;
